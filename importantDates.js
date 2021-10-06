3ce7d353d14fd7fc8578fb38db352f2b9aaa4e30
rajkumarsamanta <89797383+rajkumarsamanta@users.noreply.github.com>
2021-10-06 20:39:27 +0530

    Update importantDates.js

--- importantDates.js
+++ importantDates.js
@@ -13,7 +13,8 @@ dates = [
   { eventName: "Completed WD201 - Level 2", date: "Sept 10 2021" },
   { eventName: "Completed WD201 - Level 3", date: "Sept 15 2021" },
   { eventName: "Completed WD201 - Level 4", date: "Sept 18 2021" },
-  { eventName: "Completed WD201 - Level 5", date: "Sept 30 2021" }
+  { eventName: "Completed WD201 - Level 5", date: "Sept 30 2021" },
+  { eventName: "Completed WD201 - Level 6", date: "Oct 2 2021" }
 ];
 
 let result = "";
